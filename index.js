146c146f064ec74ed0d296406ba201743f399a65
Saurabh Khatmode <khatmodesaurabh7@gmail.com>
2023-01-15 19:53:01 +0530

    created category wise API's

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const connectToMongo = require('./config');
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const auth = require('./routes/auth');
 const cart = require('./routes/cart')
-const product=require('./routes/product')
+const product = require('./routes/product')
 connectToMongo();
 const port = 5000
+
+
+// create application/json parser
+app.use(bodyParser.json())
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(cors())
 app.use(express.json())
--- routes/product.js
+++ routes/product.js
@@ -20,5 +20,25 @@ router.get('/fetchproduct/:id', async (req, res) => {
         res.status(500).send("Internal server error")
     }
 })
+router.post('/fetchproduct/type', async (req, res) => {
+    const { userType } = req.body
+    try {
+        const product = await Product.find({ type: userType })
+        res.send(product)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error")
+    }
+})
+router.post('/fetchproduct/category', async (req, res) => {
+    const { userType, userCategory } = req.body
+    try {
+        const product = await Product.find({ type: userType, category: userCategory })
+        res.send(product)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error")
+    }
+})
 
-module.exports=router
+module.exports = router
