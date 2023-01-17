70aec0ad5c33e40257a468ccdb71dc53e76ef119
Saurabh Khatmode <khatmodesaurabh7@gmail.com>
2023-01-17 20:41:38 +0530

    created API's for custom filtering of products

--- routes/product.js
+++ routes/product.js
@@ -33,8 +33,30 @@ router.post('/fetchproduct/type', async (req, res) => {
 router.post('/fetchproduct/category', async (req, res) => {
     const { userType, userCategory } = req.body
     try {
-        const product = await Product.find({ type: userType, category: userCategory })
-        res.send(product)
+        if (userCategory == "all") {
+            const product = await Product.find({ type: userType })
+            res.send(product)
+        }
+        else if (userCategory == "pricelowtohigh") {
+            const product = await Product.find({ type: userType }).sort({ price: 1 })
+            res.send(product)
+        }
+        else if (userCategory == "pricehightolow") {
+            const product = await Product.find({ type: userType }).sort({ price: -1 })
+            res.send(product)
+        }
+        else if (userCategory == "highrated") {
+            const product = await Product.find({ type: userType }).sort({ rating: -1 })
+            res.send(product)
+        }
+        else if (userCategory == "lowrated") {
+            const product = await Product.find({ type: userType }).sort({ rating: 1 })
+            res.send(product)
+        }
+        else {
+            const product = await Product.find({ type: userType, category: userCategory })
+            res.send(product)
+        }
     } catch (error) {
         console.log(error);
         res.status(500).send("Internal server error")
