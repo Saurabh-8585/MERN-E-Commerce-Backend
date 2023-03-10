f66ccae4076fd85167233120c621e60bc2ca3585
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-03-10 16:25:02 +0530

    added already in cart msg

--- routes/cart.js
+++ routes/cart.js
@@ -1,55 +1,54 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Cart = require('../models/Cart')
-const authUser = require('../middleware/authUser')
+const Cart = require("../models/Cart");
+const authUser = require("../middleware/authUser");
 
 // get all cart products
-router.get('/fetchcart', authUser, async (req, res) => {
+router.get("/fetchcart", authUser, async (req, res) => {
     try {
-        const cart = await Cart.find({ user: req.user.id }).populate("productId", "name price image rating type").populate('user', "name email")
-        res.send(cart)
-    }
-    catch (error) {
+        const cart = await Cart.find({ user: req.user.id })
+            .populate("productId", "name price image rating type")
+            .populate("user", "name email");
+        res.send(cart);
+    } catch (error) {
         console.log(error);
-        res.status(500).send("Internal server error")
+        res.status(500).send("Internal server error");
     }
-})
+});
 
 // add to cart
 
-router.post('/addcart', authUser, async (req, res) => {
-
+router.post("/addcart", authUser, async (req, res) => {
     try {
-        const { _id, quantity } = req.body
-        const user = req.header
-        const cart = new Cart({
-            user: req.user.id, productId: _id, quantity
-        })
-        const savedCart = await cart.save()
-        res.send(savedCart)
-    }
-
-
-    catch (error) {
-        console.log(error);
-        res.status(500).send("Internal server error")
-    }
-})
-
-// remove from cart
-router.delete('/deletecart/:id', authUser, async (req, res) => {
-
-    try {
-        const result = await Cart.deleteOne({ productId: req.params.id })
-        res.send(result)
-
-
+        const { _id, quantity } = req.body;
+        const findProduct = await Cart.findOne({ productId: _id })
+        if (findProduct) {
+            return res.status(400).json({ msg: "Product already in a cart" })
+        }
+        else {
+            const user = req.header;
+            const cart = new Cart({
+                user: req.user.id,
+                productId: _id,
+                quantity,
+            });
+            const savedCart = await cart.save();
+            res.send(savedCart);
+        }
     } catch (error) {
         console.log(error);
-        res.status(500).send("Internal server error")
+        res.status(500).send("Internal server error");
     }
+});
 
-
-
-})
-module.exports = router
+// remove from cart
+router.delete("/deletecart/:id", authUser, async (req, res) => {
+    try {
+        const result = await Cart.deleteOne({ productId: req.params.id });
+        res.send(result);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error");
+    }
+});
+module.exports = router;
