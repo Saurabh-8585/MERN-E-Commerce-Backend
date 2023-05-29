f30f6add4f293a366d30621c62fc4a4f8759eb31
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-05-29 17:46:31 +0530

    added types of request

--- middleware/apiAuth.js
+++ middleware/apiAuth.js
@@ -6,11 +6,15 @@ function checkOrigin(req, res, next) {
 
     if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     } else {
         return res.status(403).json({ error: 'Forbidden' });
     }
 
     next();
 }
+
+
 
 module.exports = checkOrigin;
